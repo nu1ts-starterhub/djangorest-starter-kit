d5551c500417a7189116cedd0a2dfd2cf71fdfdd
Nikita Lugachev <nikitalugachev149th@gmail.com>
2025-09-17 00:39:04 +0700

    Auto-apply migrations on runserver start

--- manage.py
+++ manage.py
@@ -19,20 +19,29 @@ def stop_docker_compose():
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-    
-    if len(sys.argv) > 1 and sys.argv[1] in ["runserver", "migrate", "shell"]:
+
+    runserver_related = len(sys.argv) > 1 and sys.argv[1] in ["runserver", "migrate", "shell"]
+    if runserver_related:
         if os.environ.get("RUN_MAIN") or sys.argv[1] != "runserver":
             start_docker_compose()
             atexit.register(stop_docker_compose)
 
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line, call_command
+        import django
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
+    if len(sys.argv) > 1 and sys.argv[1] == "runserver":
+        if os.environ.get("RUN_MAIN"):
+            print("💾 Setting up Django...")
+            django.setup()
+            print("💾 Applying migrations automatically...")
+            call_command("migrate", interactive=False)
 
     execute_from_command_line(sys.argv)
 
